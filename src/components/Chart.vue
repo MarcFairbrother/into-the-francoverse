ffe7ce012379ac952d997690986a259128ac1bac
Marc Fairbrother <fairbrother.marc@gmail.com>
2020-03-28 15:13:43 +0100

    Manage data passed down from films component to get coordinates

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,11 +1,69 @@
 <template>
-  <div>
-    <p>Chart</p>
-  </div>
+  <svg version="1.1" baseProfile="full" xmlns="http://www.w3.org/2000/svg">
+    <g
+      v-for="(set, i) in coordinates"
+      :key="i"
+      style="stroke: #000000; stroke-width: 2;"
+    >
+      <path
+        :d="set.join(' ')"
+        style="stroke-linejoin: round; fill: none;"
+      ></path>
+    </g>
+  </svg>
 </template>
 
 <script>
 export default {
-  name: "Chart"
+  name: "Chart",
+  props: ["filmSelection"],
+  computed: {
+    coordinates: function() {
+      const data = [];
+      this.filmSelection.forEach(selection => {
+        const filmsByYear = this.listFilmsByYear(
+          1960,
+          1975,
+          selection.currentFilms
+        );
+        data.push(this.getCoordinates(filmsByYear));
+      });
+      return data;
+    }
+  },
+  methods: {
+    // creates an object listing all years from the first to last years passed as arguments
+    listAllYears: function(startYear, endYear) {
+      const years = {};
+      let currentYear = startYear;
+      while (currentYear <= endYear) {
+        years[currentYear] = [];
+        currentYear++;
+      }
+      return years;
+    },
+    // adds an array of films corresponding to each year in years object
+    listFilmsByYear: function(startYear, endYear, data) {
+      const films = this.listAllYears(startYear, endYear);
+      data.forEach(item => {
+        films[item.year].push(item.title);
+      });
+      return films;
+    },
+    // sets an array of coordinates based on an array of films for each year
+    getCoordinates: function(data) {
+      const coordinates = [];
+      let x = 10;
+      for (const property in data) {
+        if (x === 10) {
+          coordinates.push(`M${x},${100 - data[property].length * 10}`);
+        } else {
+          coordinates.push(`L${x},${100 - data[property].length * 10}`);
+        }
+        x = x + 10;
+      }
+      return coordinates;
+    }
+  }
 };
 </script>
