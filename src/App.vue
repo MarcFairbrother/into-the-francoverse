c2d43e2295e4b1f56282ec7b7d34f7023f780bc7
Marc Fairbrother <fairbrother.marc@gmail.com>
2020-05-09 16:00:53 +0200

    Implement responsive header

--- src/App.vue
+++ src/App.vue
@@ -33,39 +33,87 @@ header {
     url("./assets/img/texture_01.png"),
     linear-gradient(
       0.48turn,
-      rgba(0, 0, 0, 0.25) -20%,
+      rgba(0, 0, 0, 0.35) -20%,
       rgba(0, 0, 0, 0) 40%,
       rgba(0, 0, 0, 0) 70%,
       rgba(0, 0, 0, 0.35) 120%
     ),
     var(--pulp-red);
   color: var(--pulp-beige);
+  display: grid;
+  grid-template-columns: 1fr max-content 1fr;
   padding: 15px 15px 20px;
+  @include breakpoint($tablet-width) {
+    padding: 15px 15px 25px;
+  }
   & > h1 {
     font-family: "Changa One", Arial, Helvetica, sans-serif;
     font-variant: small-caps;
+    grid-column: 2;
+    grid-row: 1;
     margin-bottom: 20px;
-    text-shadow: -3px 3px 0 rgb(10, 10, 10), -1px -1px 0 rgb(10, 10, 10),
-      1px -1px 0 rgb(10, 10, 10), -1px 1px 0 rgb(10, 10, 10),
-      1px 1px 0 rgb(10, 10, 10);
+    text-shadow: -3px 3px 0 var(--dark-grey), -1px -1px 0 var(--dark-grey),
+      1px -1px 0 var(--dark-grey), -1px 1px 0 var(--dark-grey),
+      1px 1px 0 var(--dark-grey);
     transform: skew(-15deg, -10deg) rotate(5deg);
+    @include breakpoint($tablet-width) {
+      line-height: 0.9;
+      margin-bottom: 30px;
+      text-shadow: -5px 5px 0 var(--dark-grey), -1px -1px 0 var(--dark-grey),
+        1px -1px 0 var(--dark-grey), -1px 1px 0 var(--dark-grey),
+        1px 1px 0 var(--dark-grey);
+    }
+    @include breakpoint($desktop-width) {
+      line-height: 0.8;
+      text-shadow: -7px 7px 0 var(--dark-grey), -1px -1px 0 var(--dark-grey),
+        1px -1px 0 var(--dark-grey), -1px 1px 0 var(--dark-grey),
+        1px 1px 0 var(--dark-grey);
+    }
     & > span.into {
       font-size: 45px;
       letter-spacing: 3px;
+      @include breakpoint($tablet-width) {
+        font-size: 90px;
+        letter-spacing: 4px;
+      }
+      @include breakpoint($desktop-width) {
+        font-size: 120px;
+      }
+      @include breakpoint($large-width) {
+        font-size: 150px;
+      }
     }
     & > span.francoverse {
       font-size: 70px;
       letter-spacing: 2px;
       margin-left: 15px;
+      @include breakpoint($tablet-width) {
+        font-size: 130px;
+        letter-spacing: 4px;
+      }
+      @include breakpoint($desktop-width) {
+        font-size: 180px;
+      }
+      @include breakpoint($large-width) {
+        font-size: 200px;
+      }
     }
   }
   & > h2 {
     font-family: "Special Elite", Times, serif;
     font-size: 16px;
+    grid-column: 2;
+    grid-row: 2;
     margin-right: 10px;
     text-align: right;
-    text-shadow: -1px -1px 0 rgb(10, 10, 10), 1px -1px 0 rgb(10, 10, 10),
-      -1px 1px 0 rgb(10, 10, 10), 1px 1px 0 rgb(10, 10, 10);
+    text-shadow: -1px -1px 0 var(--dark-grey), 1px -1px 0 var(--dark-grey),
+      -1px 1px 0 var(--dark-grey), 1px 1px 0 var(--dark-grey);
+    @include breakpoint($tablet-width) {
+      font-size: 20px;
+    }
+    @include breakpoint($large-width) {
+      font-size: 24px;
+    }
   }
 }
 </style>
