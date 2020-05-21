4f5ca976f8d63693c0893c06ef1a28dde4b8b45f
Marc Fairbrother <fairbrother.marc@gmail.com>
2020-05-21 17:05:47 +0200

    Add selected filters from each selection to matching films on films list

--- src/components/FilmsList.vue
+++ src/components/FilmsList.vue
@@ -3,6 +3,15 @@
     <li v-for="(film, i) in selectedFilms" :key="i">
       <h4>{{ film.title }} ({{ film.year }})</h4>
       <p v-for="(color, n) in film.colors" :key="n">{{ color }}</p>
+      <ul>
+        <li
+          v-for="(filter, t) in film.filters"
+          :key="t"
+          :style="`background: ${filter.color}`"
+        >
+          {{ filter.type }}: {{ filter.value }}
+        </li>
+      </ul>
     </li>
   </ul>
 </template>
@@ -18,21 +27,40 @@ export default {
         selection.currentFilms.forEach(film => {
           const matchingTitle = data.find(item => item.title === film.title);
           if (matchingTitle) {
-            matchingTitle.colors.push(selection.color);
-            // Add selected filters
+            // add selection color to matching film on films list
+            matchingTitle.colors.push(Object.values(selection.color)[0]);
+            // create new filter object and add to mathcing film
+            this.addSelectedFilter(selection, matchingTitle);
           } else {
+            // create new film object to add to films list
             const newFilm = {
               title: film.title,
               year: film.year,
-              colors: [selection.color],
+              colors: [Object.values(selection.color)[0]],
               filters: []
             };
-            // Add selected filters
+            // create new filter object and add to new film
+            this.addSelectedFilter(selection, newFilm);
+            // add new film object to films list
             data.push(newFilm);
           }
         });
       });
       return data;
+    }
+  },
+  methods: {
+    addSelectedFilter: function(data, target) {
+      data.selectedFilters.forEach(filter => {
+        for (const key in filter) {
+          const newFilter = {
+            type: key,
+            value: filter[key],
+            color: Object.values(data.color)[0]
+          };
+          target.filters.push(newFilter);
+        }
+      });
     }
   }
 };
