9c1fb0dc7d2710b9b2e97be968e2b3ec218102e7
Marc Fairbrother <fairbrother.marc@gmail.com>
2020-06-20 16:40:05 +0200

    Decorative lines for desktop design

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="wrapper">
     <SiteHero />
     <SiteIntro />
     <FilmsMain />
@@ -23,4 +23,40 @@ export default {
 
 <style lang="scss">
 @import "./assets/styles/_globals.scss";
+@include breakpoint($desktop-width) {
+  .wrapper {
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    grid-auto-rows: auto;
+    &::before {
+      border-top: solid 0.5rem var(--off-white);
+      border-right: solid 0.5rem var(--off-white);
+      border-radius: 0 56px 0 0;
+      content: "";
+      display: block;
+      grid-column: 1/11;
+      grid-row: 2;
+      height: 100%;
+      transform: translateX(4rem) translateY(-1rem);
+      width: calc(100% - 4.5rem);
+      z-index: 15;
+    }
+    &::after {
+      border-top: double 1.5rem var(--off-white);
+      border-right: double 1.5rem var(--off-white);
+      border-radius: 0 70px 0 0;
+      content: "";
+      display: block;
+      grid-column: 1/11;
+      grid-row: 2;
+      height: 100%;
+      transform: translateX(2.5rem) translateY(-3rem);
+      width: calc(100% - 2rem);
+      z-index: 15;
+    }
+    & > * {
+      grid-column: 1/13;
+    }
+  }
+}
 </style>
